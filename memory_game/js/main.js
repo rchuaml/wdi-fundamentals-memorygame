cb5418e449b0b613affefa99180c1e8b6e276714
Ronnie Chua <ronniechua@ronnies-mbp.lan>
2018-11-25 21:12:47 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,26 @@
-console.log("Up and running!");
+var cards = [
+{ rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"	
+}
 
-var cards = ["queen", "queen", "king", "king"];
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function(){
@@ -14,9 +34,11 @@ alert('Sorry, try again.');
 
 var flipcard = function(cardId){
 
-console.log("User flipped " + cards[cardId]);
+console.log("User flipped " + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
-cardsInPlay.push(cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
 
 if(cardsInPlay.length === 2){
 checkForMatch();
